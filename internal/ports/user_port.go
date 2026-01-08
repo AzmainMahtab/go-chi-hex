1495d7a57441646932cbe50e0c2627c8a5aade75
Azmain Mahtab <azmainmahtab97@gmail.com>
2026-01-08 15:17:51 -0800

    service port
    service port changed from receaving and sending dto
    to receaving and sending the user domain
    to better preserve the hexagonal nature of the project

--- internal/ports/user_port.go
+++ internal/ports/user_port.go
@@ -33,22 +33,22 @@ type UserRepository interface {
 }
 
 type UserService interface {
-	RegisterUser(ctx context.Context, req dto.RegisterUserRequest) (*dto.UserResponse, error)
+	RegisterUser(ctx context.Context, req domain.User) (*domain.User, error)
 
 	// ListUsers retrieves users based on filters provided in the request.
-	ListUsers(ctx context.Context, filters map[string]any) ([]*dto.UserResponse, error)
+	ListUsers(ctx context.Context, filters map[string]any) ([]*domain.User, error)
 
 	// GetUser retrieves a single active user by their unique ID.
-	GetUser(ctx context.Context, id int) (*dto.UserResponse, error)
+	GetUser(ctx context.Context, id int) (*domain.User, error)
 
 	// UpdateUser performs a partial update on a user's information.
-	UpdateUser(ctx context.Context, id int, req dto.UpdateUserRequest) (*dto.UserResponse, error)
+	UpdateUser(ctx context.Context, id int, req dto.UpdateUserRequest) (*domain.User, error)
 
 	// RemoveUser soft-deletes a user from the active system.
 	RemoveUser(ctx context.Context, id int) error
 
 	// GetTrashedUsers retrieves users that have been soft-deleted.
-	GetTrashedUsers(ctx context.Context) ([]*dto.UserResponse, error)
+	GetTrashedUsers(ctx context.Context) ([]*domain.User, error)
 
 	// PermanentlyDeleteUser removes a user record from the database entirely.
 	PermanentlyDeleteUser(ctx context.Context, id int) error
