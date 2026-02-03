a8b3a49eb7fef764fcae43285a006d3615204372
Azmain Mahtab <azmainmahtab97@gmail.com>
2026-02-03 23:58:11 +0600

    Audir
    Audit postgres repo gone

---
+++ internal/infrastructure/postgres/audit_repo.go
@@ -0,0 +1,31 @@
+// Package postgres
+// Audit repository implementation using PostgreSQL
+package postgres
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/AzmainMahtab/go-chi-hex/internal/domain"
+	"github.com/jmoiron/sqlx"
+)
+
+type AuditRepo struct {
+	db *sqlx.DB
+}
+
+func NewAuditRepo(db *sql.DB) *AuditRepo {
+	return &AuditRepo{
+		db: sqlx.NewDb(db, "pgx"),
+	}
+}
+
+func (r *AuditRepo) Create(ctx context.Context, auditLog domain.Audit) error {
+	query := `INSERT INTO audit_logs 
+						(uuid, event_type, actor_id, payload) VALUES 
+						(:uuid, :event_type, :actor_id, :payload)`
+
+	_, err := r.db.NamedExecContext(ctx, query, auditLog)
+
+	return err
+}
